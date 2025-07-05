9aad167b74e70e865c91a0b725e191bb2863f037
NEO-O-P <neoop839@gmail.com>
2025-07-05 08:41:51 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -53,7 +53,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/NEO-O-P/EXTRA-PLUGGING",
+    "https://github.com/NEO-O-P/Extra-Plugin",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
